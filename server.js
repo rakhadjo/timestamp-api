1533c96f2d91c0af13be06f9ca74aaebfe249004
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-07-25 03:02:32 +0700

    need work on unix format

--- server.js
+++ server.js
@@ -5,6 +5,11 @@
 var express = require("express");
 var app = express();
 
+function is_utc(date_inp) {
+  let utc_regex = /\d{4}-\d{2}-\d{2}/;
+  return utc_regex.test(date_inp);
+}
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -16,6 +21,29 @@ app.use(express.static("public"));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+// basic current datetime
+app.get("/api", (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  });
+});
+
+app.get("/api/:date_str", (req, res) => {
+  let inp = req.params.date_str;
+  let x = new Date(inp);
+  if (x.toString() === "Invalid Date") {
+    res.json({
+      error: x.toString(),
+    });
+  } else {
+    res.json({
+      unix: x.getTime(),
+      utc: x.toUTCString(),
+    });
+  }
 });
 
 // your first API endpoint...
